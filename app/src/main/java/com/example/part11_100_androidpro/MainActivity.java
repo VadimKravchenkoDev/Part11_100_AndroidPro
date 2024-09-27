1ace4d590efece72c259f928b3525922f7df6cde
Arhizmei <arhizmei@gmail.com>
2024-09-27 20:26:50 +0300

    add scroll to textView

--- app/src/main/java/com/example/part11_100_androidpro/MainActivity.java
+++ app/src/main/java/com/example/part11_100_androidpro/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.part11_100_androidpro;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,42 +26,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         textLiveActivity = findViewById(R.id.textLifeActivity);
+        if(savedInstanceState != null){
+            textLiveActivity.setText(savedInstanceState.getString("textToBundle"));
+        }
         Log.d("Life of activity", "onCreate");
-        textLiveActivity.append("onCreate");
+        textLiveActivity.append("onCreate\n");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Life of activity", "onCreate");
-        textLiveActivity.append("onCreate");
+        Log.d("Life of activity", "onStart");
+        textLiveActivity.append("onCreate\n");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("Life of activity", "onRestart");
-        textLiveActivity.append("onRestart");
+        textLiveActivity.append("onRestart\n");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("Life of activity", "onPause");
-        textLiveActivity.append("onPause");
+        textLiveActivity.append("onPause\n");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("Life of activity", "onResume");
-        textLiveActivity.append("onResume");
+        textLiveActivity.append("onResume\n");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        textLiveActivity.append("onStop\n");
         Log.d("Life of activity", "onStop");
     }
 
@@ -67,5 +74,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("Life of activity", "onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        textLiveActivity.append("onSaveInstanceState()\n");
+        outState.putString("textToBundle", textLiveActivity.getText().toString());
     }
 }
