7d99c145336a765e36c47dbf8f7f3944688720e4
Arhizmei <arhizmei@gmail.com>
2024-09-27 19:14:19 +0300

    add life of activity methods

--- app/src/main/java/com/example/part11_100_androidpro/MainActivity.java
+++ app/src/main/java/com/example/part11_100_androidpro/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.part11_100_androidpro;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textLiveActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textLiveActivity = findViewById(R.id.textLifeActivity);
+        Log.d("Life of activity", "onCreate");
+        textLiveActivity.append("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Life of activity", "onCreate");
+        textLiveActivity.append("onCreate");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Life of activity", "onRestart");
+        textLiveActivity.append("onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Life of activity", "onPause");
+        textLiveActivity.append("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Life of activity", "onResume");
+        textLiveActivity.append("onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Life of activity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Life of activity", "onDestroy");
     }
 }
